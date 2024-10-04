998950919f355d1e5c27b7030d6ef3575f9e28d6
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-10-04 11:50:21 -0400

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -39,4 +39,53 @@ router.post('/users', asyncHandler(async (req, res) => {
   res.status(201).end();
 }));
 
+// fetch all courses
+app.get('/api/courses', asyncHandler(async (req, res) => {
+  const courses = await Course.findAll({
+    include: { model: User, as: 'user', attributes: ['id', 'firstName', 'lastName', 'emailAddress'] }
+  });
+  res.status(200).json(courses);
+}));
+
+// fetch course by id
+app.get('/api/courses/:id', asyncHandler(async (req, res, next) => {
+  const course = await Course.findByPk(req.params.id, {
+    include: { model: User, as: 'user', attributes: ['id', 'firstName', 'lastName', 'emailAddress'] }
+  });
+  if (course) {
+    res.status(200).json(course);
+  } else {
+    res.status(404).json({ message: 'Course not found' });
+  }
+}));
+
+//create a new course
+app.post('/api/courses', asyncHandler(async (req, res) => {
+  const course = await Course.create(req.body);
+  res.location(`/api/courses/${course.id}`).status(201).end();
+}));
+
+//update a course by id 
+app.put('/api/courses/:id', asyncHandler(async (req, res) => {
+  const course = await Course.findByPk(req.params.id);
+  if (course) {
+    await course.update(req.body);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: 'Course not found' });
+  }
+}));
+
+// delete course by id
+app.delete('/api/courses/:id', asyncHandler(async (req, res) => {
+  const course = await Course.findByPk(req.params.id);
+  if (course) {
+    await course.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: 'Course not found' });
+  }
+}));
+
+//exports 
 module.exports = router;
